03fb6a26f3a16fd89f80a1f72e26549d43ac860d
Amna-Mustafa <amna.mustafa@invozone.com>
2022-06-09 23:47:43 +0500

    Worked composition API and lifecycle hooks

---
+++ Topic9/vue-composition/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <Data />
+  <DemoOne />
+  <DemoTwo />
+  <Methods />
+  <LifecyleO />
+  <LifecycleC />
+
+</template>
+
+
+<script>
+import Data from './components/Data.vue'
+import DemoOne from './components/DemoOne.vue'
+import DemoTwo from './components/DemoTwo.vue'
+import Methods from './components/Methods.vue'
+import LifecycleO from './components/LifecycleO.vue'
+import LifecycleC from './components/LifecycleC.vue'
+
+
+
+export default {
+   name: 'App',
+   components: {
+     Data,
+     DemoOne,
+     DemoTwo,
+     Methods,
+     LifecycleC,
+     LifecycleO
+   }
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ Topic9/vue-composition/src/components/LifecycleC.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>Composition API Lifecycle Hooks</div>
+    <div>
+        <div>Life Cycle Hooks</div>
+        <div>{{ state.name }}</div>
+        <div>
+            <button @click.prevent="state.name = 'Updated InvoZone'">
+                Update Name
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+   reactive,
+} from 'vue'
+export default {
+  name: 'LifecycleC',
+  setup() {
+      const state = reactive({
+            name: 'InvoZone',
+        });
+    onBeforeMount(() => {
+      console.log('LifeCycleC onBeforeMount()')
+    })
+    onMounted(() => {
+      console.log('LifeCycleC onMounted()')
+    })
+    onBeforeUpdate(() => {
+      console.log('LifeCycleC onBeforeUpdate()')
+    })
+    onUpdated(() => {
+      console.log('LifeCycleC onUpdated()')
+    })
+    onBeforeUnmount(() => {
+      console.log('LifeCycleC onBeforeUnmount()')
+    })
+    onUnmounted(() => {
+      console.log('LifeCycleC onUnmounted()')
+    })
+    return { state };
+  },
+}
+</script>
+
+<style scoped>
+</style>
